8c5247bae9d33af242a9f70d470a15171f567ba0
Ilya Zakharov <ilushamdmaa@yandex.ru>
2022-01-18 22:48:00 +0300

    checking for user input

--- Zakharov_home_work_18_01_2022.py
+++ Zakharov_home_work_18_01_2022.py
@@ -1,2 +1,10 @@
 counter = 0
 arr_numbers = []
+while counter < 6:
+    try:
+        a = int(input("Введите число\n"))
+        if int(a):
+            arr_numbers.append(a)
+            counter += 1
+    except ValueError:
+        print("Ошибка - вы ввели не число\n")
